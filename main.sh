8cd598d603e5bba6f91ce2d2809cbccc057e150a
Markus Hartmann <markush1986@gmail.com>
2020-04-18 16:18:21 +0200

    no sudos

--- main.sh
+++ main.sh
@@ -10,7 +10,7 @@ curl -s https://raw.githubusercontent.com/rancher/k3d/master/install.sh | bash
 # Download kubectl
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/darwin/amd64/kubectl
 chmod +x ./kubectl
-sudo mv kubectl /usr/local/bin/
+mv kubectl /usr/local/bin/
 
 # Download helm
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
